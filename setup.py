f58a74cb603594d4812a49b195e186ead8bfa56e
invoid <k.baran@smsapi.pl>
2015-10-14 09:40:23 +0200

    update development status

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'requests',
     ],
     classifiers=(
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: Apache 2.0',
         'Topic :: Software Development :: Libraries :: Python Modules'
